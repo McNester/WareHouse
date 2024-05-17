88f711680144e146842a5c31664c2c72fab1db8f
McNester <seraf5@bk.ru>
2024-05-18 02:41:32 +0500

    implemented the goodsDAO for interacting with goods table

--- src/main/java/com/arta/warehouse/dao/GoodsDAO.java
+++ src/main/java/com/arta/warehouse/dao/GoodsDAO.java
@@ -1,15 +1,43 @@
 package com.arta.warehouse.dao;
 
+import com.arta.warehouse.model.Goods;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * GoodsDAO
  */
 public class GoodsDAO {
-	private static final String URL = "";
-	private static final String USER = "";
-	private static final String PASSWD = "";
-
-	public void saveGoods() {
-
+	public void saveGoods(Goods goods) {
+		String sql = "INSERT INTO goods (name, quantity) VALUES (?, ?)";
+		try (PreparedStatement stmt = DataBaseConnector.prepareStatement(sql)) {
+			stmt.setString(1, goods.getName());
+			stmt.setInt(2, goods.getQuantity());
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public List<Goods> getGoods() {
+		List<Goods> goodsList = new ArrayList<>();
+		String sql = "SELECT * FROM goods";
+		try (ResultSet rs = DataBaseConnector.get(sql)) {
+			while (rs != null && rs.next()) {
+				Goods goods = new Goods();
+				goods.setId(rs.getInt("id"));
+				goods.setName(rs.getString("name"));
+				goods.setQuantity(rs.getInt("quantity"));
+				goodsList.add(goods);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return goodsList;
+	}
 }
